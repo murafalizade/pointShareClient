bf8375b4672f649d1f5d923a39802a23b20f0d38
Murad <muradaliyev2229@gmail.com>
2024-10-10 16:35:09 +0400

    Update AppLayout.tsx

--- src/components/AppLayout.tsx
+++ src/components/AppLayout.tsx
@@ -1,32 +1,38 @@
-import {Sidebar} from "./Sidebar.tsx";
-import {ReactNode} from "react";
-import {Content} from "antd/es/layout/layout";
-import {Layout} from "antd";
-import {withAuth} from "../hoc/withAuth.tsx";
+import { Sidebar } from "./Sidebar";
+import { Content } from "antd/es/layout/layout";
+import { Layout } from "antd";
+import { withAuth } from "../hoc/withAuth";
 
 interface IAppLayoutProps {
-    children: ReactNode
+    children: any;
 }
 
-const AppLayout = ({children}:IAppLayoutProps) => {
+const AppLayout = ({ children }: IAppLayoutProps) => {
     return (
         <>
             <Layout style={{ minHeight: '100vh' }}>
                 <Sidebar />
+
                 <Layout style={{ background: "#001529" }}>
-                    <Content style={{
-                        borderRadius: "16px",
-                        background: "#1f1f1f",
-                        margin: '48px',
-                        padding: "24px",
-                        overflow: 'initial'
-                    }}>
+                    <Content
+                        style={{
+                            borderRadius: "16px",
+                            background: "#1f1f1f",
+                            margin: '48px',
+                            padding: "24px",
+                            overflow: 'initial',
+                        }}
+                    >
                         {children}
                     </Content>
                 </Layout>
+
+                <div className="bottom-navigation">
+                    <Sidebar />
+                </div>
             </Layout>
         </>
     );
 };
 
-export const AppProtectedLayout = withAuth(AppLayout)
+export const AppProtectedLayout = withAuth(AppLayout);
